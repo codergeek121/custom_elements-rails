8bfa8c94b9b2d533325d9712d16594002166a874
Niklas Haeusele <niklas.haeusele@hey.com>
2024-05-09 20:43:06 +0200

    Add observed attributes generator

--- lib/generators/custom_element_generator.rb
+++ lib/generators/custom_element_generator.rb
@@ -1,6 +1,8 @@
 class CustomElementGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
+  argument :attributes, type: :array, default: [], banner: "Observed attributes"
+
   def copy_custom_element
     template "custom_element.js", "app/javascript/custom_elements/#{file_name}_element.js"
   end
